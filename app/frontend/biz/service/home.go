75d962066136d6774571e865a7b0913bf0df5ed2
Jarvie <2981306839@qq.com>
2025-02-12 21:42:07 +0800

    基于Protobuf生成基本代码框架

--- app/frontend/biz/service/home.go
+++ app/frontend/biz/service/home.go
@@ -1,7 +1,10 @@
 package service
 
 import (
+	"GoMall/app/frontend/infra/rpc"
+	"GoMall/rpc_gen/kitex_gen/product"
 	"context"
+	"github.com/cloudwego/hertz/pkg/common/utils"
 
 	frontend_home "GoMall/app/frontend/hertz_gen/frontend_home"
 	"github.com/cloudwego/hertz/pkg/app"
@@ -17,7 +20,11 @@ func NewHomeService(Context context.Context, RequestContext *app.RequestContext)
 }
 
 func (h *HomeService) Run(req *frontend_home.Empty) (map[string]any, error) {
-
+	p, err := rpc.ProductClient.ListProducts(h.Context, &product.ListProductsReq{})
+	if err != nil {
+		return nil, err
+	}
+	return utils.H{"title": "hot sale", "items": p.Products}, nil
 	item := []map[string]any{
 		{"Name": "T-shirt", "Price": 100, "Picture": "/static/imag/logo.jpg"},
 		{"Name": "T-shirt-1", "Price": 110, "Picture": "/static/imag/logo.jpg"},
