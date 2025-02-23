e1c77a8eed638e4496bf4f593ea4d957e3fa1816
Jarvie <2981306839@qq.com>
2025-02-23 20:44:30 +0800

    基于Protobuf生成基本代码框架

--- app/product/biz/model/product.go
+++ app/product/biz/model/product.go
@@ -38,3 +38,28 @@ func NewProductQuery(ctx context.Context, db *gorm.DB) *ProductQuery {
 	}
 
 }
+
+// 添加
+func (p ProductQuery) InsertProductQuery(product Product, categoryId int32) (err error) {
+	//插入数据
+	err = p.db.WithContext(p.ctx).Create(product).Error
+	//获取自增Id
+	var id []int
+	p.db.Raw("select LAST_INSERT_ID() as id").Pluck("id", &id)
+	//新增分类_商品
+	sql := "INSERT INTO product_category (category_id, product_id) VALUES( ? ,?)"
+	p.db.WithContext(p.ctx).Exec(sql, categoryId, id[0])
+	return
+}
+
+// 修改
+func (p ProductQuery) UpdateProductQuery(product Product) (err error) {
+	err = p.db.WithContext(p.ctx).Updates(product).Error
+	return
+}
+
+// 删除
+func (p ProductQuery) DeleteProductQuery(product Product) (err error) {
+	err = p.db.WithContext(p.ctx).Delete(product).Error
+	return
+}
